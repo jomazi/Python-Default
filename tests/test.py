b680b46a4dedacd237e4d239dc713cc514ae2908
jomazi <18530806+jomazi@users.noreply.github.com>
2020-04-21 11:46:06 +0200

    Create test.py

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestTest(unittest.TestCase):
+    def test_test(self):
+        """ Sample Test Case """
+				self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
